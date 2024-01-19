d3df588305a89da0c16c2e45b4f1dbb2b7030339
asfaqMoideenSoliton <asfaqmoideen.askar@solitontech.com>
2024-01-19 12:19:14 +0530

    fix: resolve conflicts in PR, same files as branch

--- src/ExpenseTracker/Program.cs
+++ src/ExpenseTracker/Program.cs
@@ -5,8 +5,7 @@
     /// </summary>
     internal partial class Program
     {
-        private static ExpenseTracker _expenseTracker = new ExpenseTracker();
-        private static IncomeTracker _incomeTracker = new IncomeTracker();
+        private static FinanceTracker _incometracker = new FinanceTracker();
 
         private static void Main(string[] args)
         {
@@ -16,33 +15,38 @@
             {
                 Console.WriteLine("----------------------------------------------------------\n");
                 Console.WriteLine("Please find the list of operations offered below:");
-                Console.WriteLine("1. Add an expense \n2. Add an Income \n3. Show History\n4. Edit a Income Expense\n5. Show Financial Summary\n0. Exit application\n");
+                Console.WriteLine("1. Add an expense \n2. Add an Income \n3. Show History\n4. Edit a Income/Expense\n5. Show Financial Summary \n6. Delete Income/Expense\n Q - Exit application\n");
                 Console.Write("Choose one of the above listed option to proceed: ) ");
                 option = Console.ReadLine();
-                ChooseTheOption(option);
+                PerformOperation(option);
             }
             while (option != "q" && option != "Q");
         }
-        private static void ChooseTheOption(string option)
+
+        private static void PerformOperation(string option)
         {
             switch (option)
             {
                 case "1":
-                    _expenseTracker.AddExpense();
+                    _incometracker.AddExpense();
                     break;
                 case "2":
-                    _incomeTracker.AddIncome();
+                    _incometracker.AddIncome();
                     break;
-               
+
                 case "3":
-                    ShowRecord();
+                    _incometracker.ShowRecord();
+                    break;
+                case "4":
+                    _incometracker.EditRecord();
+                    break;
+                case "5":
+                    _incometracker.GenerateFinancialSummary();
+                    break;
+                case "6":
+                    _incometracker.DeleteRecord();
                     break;
             }
-        }
-        private static void ShowRecord()
-        {
-            _expenseTracker.ViewExpense();
-            _incomeTracker.ViewIncome();
         }
     }
 }
